6453213eb584ac23696356fe360aad84904f13ad
OwlCJ <pcj0308@gmail.com>
2023-05-21 00:30:25 +0900

    Add: Onboarding Pages Adding

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/tutotial_screen.dart';
 import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
@@ -68,6 +69,14 @@ class _InterestsScreenState extends State<InterestsScreen> {
         _showTitle = false;
       });
     }
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const TutorialScreen(),
+        ));
   }
 
   @override
@@ -146,7 +155,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
               right: Sizes.size24,
             ),
             child: CupertinoButton(
-              onPressed: () {},
+              onPressed: _onNextTap,
               color: Theme.of(context).primaryColor,
               child: const Text('Next'),
             )),
