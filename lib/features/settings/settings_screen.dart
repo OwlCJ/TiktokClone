4d905ba1a84203c0d6a7121c699fc14194064e98
OwlCJ <pcj0308@gmail.com>
2023-05-29 01:17:21 +0900

    Fix: temporary comment out unused code

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -40,36 +40,36 @@ class _SettingsScreenState extends State<SettingsScreen> {
             title: const Text("Marketing emails"),
             subtitle: const Text("We won't spam you."),
           ),
-          ListTile(
-            onTap: () async {
-              final date = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1980),
-                lastDate: DateTime(2030),
-              );
-              final time = await showTimePicker(
-                context: context,
-                initialTime: TimeOfDay.now(),
-              );
-              final booking = await showDateRangePicker(
-                context: context,
-                firstDate: DateTime(1980),
-                lastDate: DateTime(2030),
-                builder: (context, child) {
-                  return Theme(
-                    data: ThemeData(
-                        appBarTheme: const AppBarTheme(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.black)),
-                    child: child!,
-                  );
-                },
-              );
-            },
-            title: const Text("What is your birthday?"),
-            subtitle: const Text("I need to know!"),
-          ),
+          // ListTile(
+          //   onTap: () async {
+          //     final date = await showDatePicker(
+          //       context: context,
+          //       initialDate: DateTime.now(),
+          //       firstDate: DateTime(1980),
+          //       lastDate: DateTime(2030),
+          //     );
+          //     final time = await showTimePicker(
+          //       context: context,
+          //       initialTime: TimeOfDay.now(),
+          //     );
+          //     final booking = await showDateRangePicker(
+          //       context: context,
+          //       firstDate: DateTime(1980),
+          //       lastDate: DateTime(2030),
+          //       builder: (context, child) {
+          //         return Theme(
+          //           data: ThemeData(
+          //               appBarTheme: const AppBarTheme(
+          //                   foregroundColor: Colors.white,
+          //                   backgroundColor: Colors.black)),
+          //           child: child!,
+          //         );
+          //       },
+          //     );
+          //   },
+          //   title: const Text("What is your birthday?"),
+          //   subtitle: const Text("I need to know!"),
+          // ),
           ListTile(
             title: const Text("Log out (iOS)"),
             textColor: Colors.red,
