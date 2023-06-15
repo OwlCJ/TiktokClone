29e3ecbe5751e99fd4bf9dda47f2e8c4d1283ccd
OwlCJ <pcj0308@gmail.com>
2023-06-15 13:26:20 +0900

    Add: config_vm riverpod converting

--- lib/features/videos/view_models/playback_config_vm.dart
+++ lib/features/videos/view_models/playback_config_vm.dart
@@ -1,30 +1,40 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/videos/models/playback_config_model.dart';
 import 'package:tiktok_clone/features/videos/repos/video_playback_config_repo.dart';
 
-class PlaybackConfigViewModel extends ChangeNotifier {
+class PlaybackConfigViewModel extends Notifier<PlaybackConfigModel> {
   final VideoPlaybackConfigRepository _repository;
-
-  late final PlaybackConfigModel _model = PlaybackConfigModel(
-    muted: _repository.isMuted(),
-    autoplay: _repository.isAutoplay(),
-  );
 
   PlaybackConfigViewModel(this._repository);
 
-  bool get muted => _model.muted;
-
-  bool get autoplay => _model.autoplay;
-
   void setMuted(bool value) {
     _repository.setMuted(value);
-    _model.muted = value;
-    notifyListeners();
+    state = PlaybackConfigModel(
+      muted: value,
+      autoplay: state.autoplay,
+    );
   }
 
+  // State 안에 포함된 데이터를 직접 변경하지 않고 새로 부여해줌 (immutable 특성유지)
   void setAutoplay(bool value) {
     _repository.setAutoplay(value);
-    _model.autoplay = value;
-    notifyListeners();
+    state = PlaybackConfigModel(
+      muted: state.muted,
+      autoplay: value,
+    );
+  }
+
+  @override
+  PlaybackConfigModel build() {
+    // riverpod 의 build 는 state값의 초기값을 설정
+    return PlaybackConfigModel(
+      muted: _repository.isMuted(),
+      autoplay: _repository.isAutoplay(),
+    );
   }
 }
+
+final plyabackConfigProvider =
+    NotifierProvider<PlaybackConfigViewModel, PlaybackConfigModel>(
+  () => throw UnimplementedError(),
+);
