cc7276f94052e85aa81f9a24df260235421c8dba
OwlCJ <pcj0308@gmail.com>
2023-06-20 10:01:49 +0900

    Fix: views 경로 변경 refactoring

--- lib/features/authentication/views/sign_up_screen.dart
+++ lib/features/authentication/views/sign_up_screen.dart
@@ -3,9 +3,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
+import 'package:tiktok_clone/features/authentication/views/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/views/username_screen.dart';
+import 'package:tiktok_clone/features/authentication/views/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String routeURL = '/';
